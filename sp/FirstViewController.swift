27d0614172060fa9cd29fff258dfea60f47985ce
Damisa Tangprasobsang <ori_@outlook.co.th>
2018-09-08 14:44:13 +0700

    Initial Commit

---
+++ sp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  sp
+//
+//  Created by Damisa Tangprasobsang on 8/9/2561 BE.
+//  Copyright © 2561 Damisa Tangprasobsang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  sp
+//
+//  Created by Damisa Tangprasobsang on 8/9/2561 BE.
+//  Copyright © 2561 Damisa Tangprasobsang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
