bf27695803e50e5615a358c97759fb8f8b9c220f
chanchana <north1602@gmail.com>
2018-09-09 10:54:21 +0700

    first

---
+++ sp/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  sp
+//
+//  Created by Damisa Tangprasobsang on 8/9/2561 BE.
+//  Copyright © 2561 Damisa Tangprasobsang. All rights reserved.
+//
+
+import UIKit
+
+class Data: NSObject {
+
+}
---
+++ sp/RedeemCell.swift
@@ -0,0 +1,24 @@
+//
+//  RedeemCell.swift
+//  sp
+//
+//  Created by Damisa Tangprasobsang on 8/9/2561 BE.
+//  Copyright © 2561 Damisa Tangprasobsang. All rights reserved.
+//
+
+import UIKit
+
+class RedeemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
